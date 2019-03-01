de08f683b900daf6954fa1dee770a4a6fa970235
Bright Olayinka <explorebright@gmail.com>
2019-03-01 02:58:08 +0000

    Automatically backed up by Learn

--- arrays.js
+++ arrays.js
@@ -1,3 +1,8 @@
 var chocolateBars = ["snickers", "hundred grand", "kitkat", "skittles"];
-chocolateBars.push("rice");
-chocolateBars.unshift("beans")
+
+function addElementToBeginningOfArray(){
+  
+}
+function destructivelyAddElementToBeginningOfArray(){
+  
+}
