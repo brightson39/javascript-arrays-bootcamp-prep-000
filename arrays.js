02b9986eb7a525fbba8f138a8755446565501380
Bright Olayinka <explorebright@gmail.com>
2019-03-01 03:05:56 +0000

    Automatically backed up by Learn

--- arrays.js
+++ arrays.js
@@ -5,37 +5,27 @@ function addElementToBeginningOfArray(array, element){
   return new_array
 }
  
- function destructivelyAddElementToBeginningOfArray(array, element) {
+ function descructivelyAddElementToBeginningOfArray(array, element){
   array.unshift(element)
   return array
 }
-
- function addElementToEndOfArray(array, element) {
-  return [...array, element]
-}
-
- function destructivelyAddElementToEndOfArray(array, element) {
+function addElementToEndOfArray(array, element){
   array.push(element)
   return array
 }
+function destructivelyAddElementToEndOfArray(array, element){
+  array.push(element)
+  return array
+}
+function accessElementInArray(array, index){
 
- function destructivelyRemoveElementFromBeginningOfArray(array) {
+   return array[2]
+}
+function removeElementFromBeginningOfArray(array){
   array.shift()
-  array
+  return array
 }
-
- function removeElementFromBeginningOfArray(array) {
-  return array.slice(1)
-}
-
- function destructivelyRemoveElementFromEndOfArray(array) {
+function removeElementFromEndOfArray(array){
   array.pop()
   return array
 }
-
- function removeElementFromEndOfArray(array) {
-  return array.slice(0, array.length - 1)
-}
-
- function accessElementInArray(array, index) {
-  return array[index]
