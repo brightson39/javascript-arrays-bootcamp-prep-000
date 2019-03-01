7a8af6b01b57ff3f9ccc0ebcf3e44409b723a210
Bright Olayinka <explorebright@gmail.com>
2019-03-01 03:04:46 +0000

    Automatically backed up by Learn

--- arrays.js
+++ arrays.js
@@ -2,6 +2,7 @@ var chocolateBars = ["Snickers", "hundred grand", "kitkat", "skittles"];
 
 function addElementToBeginningOfArray(array, element){
   var new_array = [element, array[0]]
+  return new_array
   
  function addElementToBeginningOfArray(array, element) {
   return [element, ...array]
